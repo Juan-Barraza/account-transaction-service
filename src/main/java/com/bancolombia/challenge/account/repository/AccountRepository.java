630d57e0a791dc2123fdaeaba662c1a19dceb52c
Juan-Barraza <juanbarraza2004@outlook.com>
2026-09-18 10:10:20 -0500

    feat: implement a dto to transfer controlated errors and exceptions with the central or global exception handler to return the contraled exceptions

---
+++ src/main/java/com/bancolombia/challenge/account/repository/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.bancolombia.challenge.account.repository;
+
+import com.bancolombia.challenge.account.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Optional<Account> findByAccountNumber(String accountNumber);
+    boolean existAccountNumber(String accountNumber);
+}
---
+++ src/main/java/com/bancolombia/challenge/account/repository/TransactionRepository.java
@@ -0,0 +1,15 @@
+package com.bancolombia.challenge.account.repository;
+
+
+import com.bancolombia.challenge.account.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    Page<Transaction> findByAccountId(Long accountId, Pageable pageable);
+}
